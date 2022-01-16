5cc07ebb055d29a67bc905d29404b6b8b266dcd1
nakajima-satoru <nakajima@email.com>
2022-01-16 17:27:40 +0900

    1/16

--- index.js
+++ index.js
@@ -22,17 +22,6 @@ module.exports = function(conf, context){
     this.fookRequest = function(resolve, req, res){
 
 		try{
-
-			res.setHeader("server","hachiware server");
-
-			if(conf.headers){
-				var colums = Object.keys(conf.headers);
-				for(var n = 0 ; n < colums.length ; n++){
-					var field = colums[n];
-					var value = conf.headers[field];
-					res.setHeader(field, value);
-				}
-			}
 	
             context.loadFookModule("access",[
                 req, 
